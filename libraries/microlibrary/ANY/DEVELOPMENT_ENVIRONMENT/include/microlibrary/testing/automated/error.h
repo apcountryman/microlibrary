af435220899f7d978f78a7787dc3c59a39712a2a
Andrew Countryman <apcountryman@gmail.com>
2024-04-23 21:19:42 -0700

    Add error code
    Resolves #108 (Add error code).

--- libraries/microlibrary/ANY/DEVELOPMENT_ENVIRONMENT/include/microlibrary/testing/automated/error.h
+++ libraries/microlibrary/ANY/DEVELOPMENT_ENVIRONMENT/include/microlibrary/testing/automated/error.h
@@ -23,10 +23,47 @@
 #ifndef MICROLIBRARY_TESTING_AUTOMATED_AUTOMATED_ERROR_H
 #define MICROLIBRARY_TESTING_AUTOMATED_AUTOMATED_ERROR_H
 
+#include <ostream>
+#include <type_traits>
+
 #include "gmock/gmock.h"
+#include "microlibrary/enum.h"
 #include "microlibrary/error.h"
 
+namespace microlibrary {
+
+/**
+ * \brief Insertion operator.
+ *
+ * \param[in] stream The stream to write the microlibrary::Error_Code to.
+ * \param[in] error The microlibrary::Error_Code to write to the stream.
+ *
+ * \return stream
+ */
+auto operator<<( std::ostream & stream, Error_Code const & error ) -> std::ostream &;
+
+} // namespace microlibrary
+
 namespace microlibrary::Testing::Automated {
+
+/**
+ * \brief Mock errors.
+ *
+ * \relatedalso microlibrary::Testing::Automated::Mock_Error_Category
+ */
+enum class Mock_Error : Error_ID {};
+
+/**
+ * \brief Insertion operator.
+ *
+ * \param[in] stream The stream to write the microlibrary::Testing::Automated::Mock_Error
+ *            to.
+ * \param[in] error The microlibrary::Testing::Automated::Mock_Error to write to the
+ *            stream.
+ *
+ * \return stream
+ */
+auto operator<<( std::ostream & stream, Mock_Error error ) -> std::ostream &;
 
 /**
  * \brief Mock error category.
@@ -57,6 +94,33 @@ class Mock_Error_Category : public Error_Category {
     MOCK_METHOD( char const *, error_description, ( Error_ID ), ( const, noexcept, override ) );
 };
 
+/**
+ * \brief Construct an error code from a mock error.
+ *
+ * \relatedalso microlibrary::Testing::Automated::Mock_Error_Category
+ *
+ * \param[in] error The mock error to construct the error code from.
+ *
+ * \return The constructed error code.
+ */
+inline auto make_error_code( Mock_Error error ) noexcept -> Error_Code
+{
+    return { Mock_Error_Category::instance(), to_underlying( error ) };
+}
+
 } // namespace microlibrary::Testing::Automated
+
+namespace microlibrary {
+
+/**
+ * \brief microlibrary::Testing::Automated::Mock_Error error code enum registration.
+ *
+ * \relatedalso microlibrary::Testing::Automated::Mock_Error_Category
+ */
+template<>
+struct is_error_code_enum<Testing::Automated::Mock_Error> : std::true_type {
+};
+
+} // namespace microlibrary
 
 #endif // MICROLIBRARY_TESTING_AUTOMATED_AUTOMATED_ERROR_H
