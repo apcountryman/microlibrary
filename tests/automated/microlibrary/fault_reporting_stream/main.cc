1cbefa907e9b040ea9d0daadd7aed9bfd91f82f3
Andrew Countryman <apcountryman@gmail.com>
2024-06-20 15:01:07 -0700

    Add fault reporting I/O stream core (#206)
    Resolves #204 (Add fault reporting I/O stream core).

---
+++ tests/automated/microlibrary/fault_reporting_stream/main.cc
@@ -0,0 +1,250 @@
+/**
+ * microlibrary
+ *
+ * Copyright 2024, Andrew Countryman <apcountryman@gmail.com> and the microlibrary
+ * contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+/**
+ * \file
+ * \brief microlibrary::Fault_Reporting_Stream automated tests.
+ */
+
+#include <ios>
+#include <ostream>
+
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+#include "microlibrary/stream.h"
+#include "microlibrary/testing/automated/stream.h"
+
+namespace {
+
+using ::microlibrary::Testing::Automated::Mock_Fault_Reporting_Stream_IO_Driver;
+using ::testing::TestWithParam;
+using ::testing::ValuesIn;
+
+class Fault_Reporting_Stream : public ::microlibrary::Fault_Reporting_Stream {
+  public:
+    using ::microlibrary::Fault_Reporting_Stream::Fault_Reporting_Stream;
+
+    ~Fault_Reporting_Stream() noexcept = default;
+
+    constexpr auto operator         =( Fault_Reporting_Stream && expression ) noexcept
+        -> Fault_Reporting_Stream & = default;
+
+    constexpr auto operator=( Fault_Reporting_Stream const & expression ) noexcept
+        -> Fault_Reporting_Stream & = default;
+
+    using ::microlibrary::Fault_Reporting_Stream::clear_end_of_file_reached_report;
+    using ::microlibrary::Fault_Reporting_Stream::report_end_of_file_reached;
+
+    using ::microlibrary::Fault_Reporting_Stream::clear_fatal_error_report;
+    using ::microlibrary::Fault_Reporting_Stream::report_fatal_error;
+
+    using ::microlibrary::Fault_Reporting_Stream::driver;
+    using ::microlibrary::Fault_Reporting_Stream::set_driver;
+};
+
+} // namespace
+
+/**
+ * \brief microlibrary::Fault_Reporting_Stream::is_nominal(),
+ *        microlibrary::Fault_Reporting_Stream::error_reported(),
+ *        microlibrary::Fault_Reporting_Stream::end_of_file_reached(),
+ *        microlibrary::Fault_Reporting_Stream::report_end_of_file_reached(),
+ *        microlibrary::Fault_Reporting_Stream::clear_end_of_file_reached_report(),
+ *        microlibrary::Fault_Reporting_Stream::io_error_reported(),
+ *        microlibrary::Fault_Reporting_Stream::report_io_error(),
+ *        microlibrary::Fault_Reporting_Stream::clear_io_error_report(),
+ *        microlibrary::Fault_Reporting_Stream::fatal_error_reported(),
+ *        microlibrary::Fault_Reporting_Stream::report_fatal_error(), and
+ *        microlibrary::Fault_Reporting_Stream::clear_fatal_error_report() test case.
+ */
+struct flagSetClear_Test_Case {
+    /**
+     * \brief Flag manipulation function pointer.
+     */
+    using Flag_Manipulation_Function_Pointer = void ( Fault_Reporting_Stream::* )();
+
+    /**
+     * \brief The function to call to set the flag.
+     */
+    Flag_Manipulation_Function_Pointer set_flag;
+
+    /**
+     * \brief The function to call to clear the flag.
+     */
+    Flag_Manipulation_Function_Pointer clear_flag;
+
+    /**
+     * \brief Errors have been reported.
+     */
+    bool error_reported;
+
+    /**
+     * \brief End-of-file has been reached.
+     */
+    bool end_of_file_reached;
+
+    /**
+     * \brief I/O error has been reported.
+     */
+    bool io_error_reported;
+
+    /**
+     * \brief Fatal error (fault) has been reported.
+     */
+    bool fatal_error_reported;
+};
+
+auto operator<<( std::ostream & stream, flagSetClear_Test_Case const & test_case ) -> std::ostream &
+{
+    // clang-format off
+
+    return stream << "{ "
+                  << ".error_reported = " << std::boolalpha << test_case.error_reported
+                  << ", "
+                  << ".end_of_file_reached = " << std::boolalpha << test_case.end_of_file_reached
+                  << ", "
+                  << ".io_error_reported = " << std::boolalpha << test_case.io_error_reported
+                  << ", "
+                  << ".fatal_error_reported = " << std::boolalpha << test_case.fatal_error_reported
+                  << " }";
+
+    // clang-format on
+}
+
+/**
+ * \brief microlibrary::Fault_Reporting_Stream::is_nominal(),
+ *        microlibrary::Fault_Reporting_Stream::error_reported(),
+ *        microlibrary::Fault_Reporting_Stream::end_of_file_reached(),
+ *        microlibrary::Fault_Reporting_Stream::report_end_of_file_reached(),
+ *        microlibrary::Fault_Reporting_Stream::clear_end_of_file_reached_report(),
+ *        microlibrary::Fault_Reporting_Stream::io_error_reported(),
+ *        microlibrary::Fault_Reporting_Stream::report_io_error(),
+ *        microlibrary::Fault_Reporting_Stream::clear_io_error_report(),
+ *        microlibrary::Fault_Reporting_Stream::fatal_error_reported(),
+ *        microlibrary::Fault_Reporting_Stream::report_fatal_error(), and
+ *        microlibrary::Fault_Reporting_Stream::clear_fatal_error_report() test fixture.
+ */
+class flagSetClear : public TestWithParam<flagSetClear_Test_Case> {
+};
+
+/**
+ * \brief Verify microlibrary::Fault_Reporting_Stream::is_nominal(),
+ *        microlibrary::Fault_Reporting_Stream::error_reported(),
+ *        microlibrary::Fault_Reporting_Stream::end_of_file_reached(),
+ *        microlibrary::Fault_Reporting_Stream::report_end_of_file_reached(),
+ *        microlibrary::Fault_Reporting_Stream::clear_end_of_file_reached_report(),
+ *        microlibrary::Fault_Reporting_Stream::io_error_reported(),
+ *        microlibrary::Fault_Reporting_Stream::report_io_error(),
+ *        microlibrary::Fault_Reporting_Stream::clear_io_error_report(),
+ *        microlibrary::Fault_Reporting_Stream::fatal_error_reported(),
+ *        microlibrary::Fault_Reporting_Stream::report_fatal_error(), and
+ *        microlibrary::Fault_Reporting_Stream::clear_fatal_error_report() work properly.
+ */
+TEST_P( flagSetClear, worksProperly )
+{
+    auto const test_case = GetParam();
+
+    auto stream = Fault_Reporting_Stream{};
+
+    EXPECT_TRUE( stream.is_nominal() );
+    EXPECT_FALSE( stream.error_reported() );
+    EXPECT_FALSE( stream.end_of_file_reached() );
+    EXPECT_FALSE( stream.io_error_reported() );
+    EXPECT_FALSE( stream.fatal_error_reported() );
+
+    ( stream.*( test_case.clear_flag ) )();
+
+    EXPECT_TRUE( stream.is_nominal() );
+    EXPECT_FALSE( stream.error_reported() );
+    EXPECT_FALSE( stream.end_of_file_reached() );
+    EXPECT_FALSE( stream.io_error_reported() );
+    EXPECT_FALSE( stream.fatal_error_reported() );
+
+    ( stream.*( test_case.set_flag ) )();
+
+    EXPECT_FALSE( stream.is_nominal() );
+    EXPECT_EQ( stream.error_reported(), test_case.error_reported );
+    EXPECT_EQ( stream.end_of_file_reached(), test_case.end_of_file_reached );
+    EXPECT_EQ( stream.io_error_reported(), test_case.io_error_reported );
+    EXPECT_EQ( stream.fatal_error_reported(), test_case.fatal_error_reported );
+
+    ( stream.*( test_case.set_flag ) )();
+
+    EXPECT_FALSE( stream.is_nominal() );
+    EXPECT_EQ( stream.error_reported(), test_case.error_reported );
+    EXPECT_EQ( stream.end_of_file_reached(), test_case.end_of_file_reached );
+    EXPECT_EQ( stream.io_error_reported(), test_case.io_error_reported );
+    EXPECT_EQ( stream.fatal_error_reported(), test_case.fatal_error_reported );
+
+    ( stream.*( test_case.clear_flag ) )();
+
+    EXPECT_TRUE( stream.is_nominal() );
+    EXPECT_FALSE( stream.error_reported() );
+    EXPECT_FALSE( stream.end_of_file_reached() );
+    EXPECT_FALSE( stream.io_error_reported() );
+    EXPECT_FALSE( stream.fatal_error_reported() );
+}
+
+/**
+ * \brief microlibrary::Fault_Reporting_Stream::is_nominal(),
+ *        microlibrary::Fault_Reporting_Stream::error_reported(),
+ *        microlibrary::Fault_Reporting_Stream::end_of_file_reached(),
+ *        microlibrary::Fault_Reporting_Stream::report_end_of_file_reached(),
+ *        microlibrary::Fault_Reporting_Stream::clear_end_of_file_reached_report(),
+ *        microlibrary::Fault_Reporting_Stream::io_error_reported(),
+ *        microlibrary::Fault_Reporting_Stream::report_io_error(),
+ *        microlibrary::Fault_Reporting_Stream::clear_io_error_report(),
+ *        microlibrary::Fault_Reporting_Stream::fatal_error_reported(),
+ *        microlibrary::Fault_Reporting_Stream::report_fatal_error(), and
+ *        microlibrary::Fault_Reporting_Stream::clear_fatal_error_report() test cases.
+ */
+flagSetClear_Test_Case const flagSetClear_TEST_CASES[]{
+    // clang-format off
+
+    { &Fault_Reporting_Stream::report_end_of_file_reached, &Fault_Reporting_Stream::clear_end_of_file_reached_report, false,  true, false, false },
+    { &Fault_Reporting_Stream::report_io_error,            &Fault_Reporting_Stream::clear_io_error_report,             true, false,  true, false },
+    { &Fault_Reporting_Stream::report_fatal_error,         &Fault_Reporting_Stream::clear_fatal_error_report,          true, false, false,  true },
+
+    // clang-format on
+};
+
+INSTANTIATE_TEST_SUITE_P(, flagSetClear, ValuesIn( flagSetClear_TEST_CASES ) );
+
+/**
+ * \brief Verify microlibrary::Fault_Reporting_Stream::driver_is_set(),
+ *        microlibrary::Fault_Reporting_Stream::driver(), and
+ *        microlibrary::Fault_Reporting_Stream::set_driver() work properly.
+ */
+TEST( driver, worksProperly )
+{
+    auto stream = Fault_Reporting_Stream{};
+
+    EXPECT_FALSE( stream.driver_is_set() );
+    EXPECT_EQ( stream.driver(), nullptr );
+
+    auto driver = Mock_Fault_Reporting_Stream_IO_Driver{};
+
+    stream.set_driver( &driver );
+
+    EXPECT_TRUE( stream.driver_is_set() );
+    EXPECT_EQ( stream.driver(), &driver );
+
+    stream.set_driver( nullptr );
+
+    EXPECT_FALSE( stream.driver_is_set() );
+    EXPECT_EQ( stream.driver(), nullptr );
+}
