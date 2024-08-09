98eea93712c1e14314945577c6d54786f22a6212
Andrew Countryman <apcountryman@gmail.com>
2024-08-09 14:34:42 -0700

    Add register
    Resolves #255 (Add register).

--- docs/register.md
+++ docs/register.md
@@ -5,3 +5,23 @@ Register facilities are defined in the `microlibrary` static library's
 header/source file pair.
 
 ## Table of Contents
+
+1. [Register](#register)
+
+## Register
+
+The `::microlibrary::Register` class is used to declare a microcontroller register.
+Register access is restricted to the following operations:
+- Conversion to the register's underlying integer type (reading the register)
+- Assignment
+- Bitwise AND assignment
+- Bitwise OR assignment
+- Bitwise XOR assignment
+
+The `::microlibrary::Testing::Automated::Mock_Register` mock register class is available
+if `MICROLIBRARY_TARGET` is `DEVELOPMENT_ENVIRONMENT`.
+`::microlibrary::Register` is an alias for
+`::microlibrary::Testing::Automated::Mock_Register` when it is available.
+The mock is defined in the
+[`microlibrary/testing/automated/register.h`](https://github.com/apcountryman/microlibrary/blob/main/libraries/microlibrary/ANY/DEVELOPMENT_ENVIRONMENT/include/microlibrary/testing/automated/register.h)/[`microlibrary/testing/automated/register.cc`](https://github.com/apcountryman/microlibrary/blob/main/libraries/microlibrary/ANY/DEVELOPMENT_ENVIRONMENT/source/microlibrary/testing/automated/register.cc)
+header/source file pair.
--- libraries/microlibrary/ANY/DEVELOPMENT_ENVIRONMENT/include/microlibrary/testing/automated/register.h
+++ libraries/microlibrary/ANY/DEVELOPMENT_ENVIRONMENT/include/microlibrary/testing/automated/register.h
@@ -23,7 +23,77 @@
 #ifndef MICROLIBRARY_TESTING_AUTOMATED_REGISTER_H
 #define MICROLIBRARY_TESTING_AUTOMATED_REGISTER_H
 
+#include <type_traits>
+
+#include "gmock/gmock.h"
+
 namespace microlibrary::Testing::Automated {
+
+/**
+ * \brief Mock register.
+ *
+ * \tparam T The register's underlying integer type.
+ */
+template<typename T>
+class Mock_Register {
+  public:
+    static_assert( std::is_integral_v<T> );
+
+    using Type = T;
+
+    Mock_Register() = default;
+
+    Mock_Register( Mock_Register && ) = delete;
+
+    Mock_Register( Mock_Register const & ) = delete;
+
+    ~Mock_Register() noexcept = default;
+
+    auto operator=( Mock_Register && ) = delete;
+
+    auto operator=( Mock_Register const & ) = delete;
+
+    MOCK_METHOD( Type, read, () );
+
+    MOCK_METHOD( void, write, ( Type ) );
+    MOCK_METHOD( void, write_and, ( Type ) );
+    MOCK_METHOD( void, write_or, ( Type ) );
+    MOCK_METHOD( void, write_xor, ( Type ) );
+
+    operator Type() const noexcept
+    {
+        return read();
+    }
+
+    auto operator=( Type expression ) noexcept -> Mock_Register &
+    {
+        write( expression );
+
+        return *this;
+    }
+
+    auto operator&=( Type expression ) noexcept -> Mock_Register &
+    {
+        write_and( expression );
+
+        return *this;
+    }
+
+    auto operator|=( Type expression ) noexcept -> Mock_Register &
+    {
+        write_or( expression );
+
+        return *this;
+    }
+
+    auto operator^=( Type expression ) noexcept -> Mock_Register &
+    {
+        write_xor( expression );
+
+        return *this;
+    }
+};
+
 } // namespace microlibrary::Testing::Automated
 
 #endif // MICROLIBRARY_TESTING_AUTOMATED_REGISTER_H
