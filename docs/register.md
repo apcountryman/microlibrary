d54dc48484df045e32241564aa16422dcd28883a
Andrew Countryman <apcountryman@gmail.com>
2024-08-09 18:58:50 -0700

    Add register (#260)
    Resolves #255 (Add register).

--- docs/register.md
+++ docs/register.md
@@ -5,3 +5,24 @@ Register facilities are defined in the `microlibrary` static library's
 header/source file pair.
 
 ## Table of Contents
+
+1. [Register](#register)
+
+## Register
+
+The `::microlibrary::Register` class is used to declare a microcontroller register.
+Register access is restricted to the following operations:
+- Conversion to the register's underlying integer type (reading the register)
+- Assignment
+- Bitwise AND assignment
+- Bitwise OR assignment
+- Bitwise XOR assignment
+
+The `::microlibrary::Testing::Automated::Mock_Register` mock register class is available
+if `MICROLIBRARY_TARGET` is `DEVELOPMENT_ENVIRONMENT`.
+The mock is defined in the
+[`microlibrary/testing/automated/register.h`](https://github.com/apcountryman/microlibrary/blob/main/libraries/microlibrary/ANY/DEVELOPMENT_ENVIRONMENT/include/microlibrary/testing/automated/register.h)/[`microlibrary/testing/automated/register.cc`](https://github.com/apcountryman/microlibrary/blob/main/libraries/microlibrary/ANY/DEVELOPMENT_ENVIRONMENT/source/microlibrary/testing/automated/register.cc)
+header/source file pair.
+`::microlibrary::Register` is an alias for the
+`::microlibrary::Testing::Automated::Mock_Register` when `MICROLIBRARY_TARGET` is
+`DEVELOPMENT_ENVIRONMENT`.
--- libraries/microlibrary/ANY/ANY/include/microlibrary/register.h
+++ libraries/microlibrary/ANY/ANY/include/microlibrary/register.h
@@ -23,7 +23,124 @@
 #ifndef MICROLIBRARY_REGISTER_H
 #define MICROLIBRARY_REGISTER_H
 
+#include <type_traits>
+
+#if MICROLIBRARY_TARGET_IS_DEVELOPMENT_ENVIRONMENT
+#include "microlibrary/testing/automated/register.h"
+#endif // MICROLIBRARY_TARGET_IS_DEVELOPMENT_ENVIRONMENT
+
 namespace microlibrary {
+
+#if MICROLIBRARY_TARGET_IS_HARDWARE
+#define MICROLIBRARY_PACKED_REGISTER __attribute__( ( __packed__ ) )
+#else // MICROLIBRARY_TARGET_IS_HARDWARE
+#define MICROLIBRARY_PACKED_REGISTER
+#endif // MICROLIBRARY_TARGET_IS_HARDWARE
+
+/**
+ * \brief Register.
+ *
+ * \tparam T The register's underlying integer type.
+ */
+template<typename T>
+#if MICROLIBRARY_TARGET_IS_DEVELOPMENT_ENVIRONMENT
+using Register = Testing::Automated::Mock_Register<T>;
+#else  // MICROLIBRARY_TARGET_IS_DEVELOPMENT_ENVIRONMENT
+class MICROLIBRARY_PACKED_REGISTER Register {
+  public:
+    static_assert( std::is_integral_v<T> );
+
+    /**
+     * \brief The register's underlying integer type.
+     */
+    using Type = T;
+
+    Register() = delete;
+
+    Register( Register && ) = delete;
+
+    Register( Register const & ) = delete;
+
+    ~Register() = delete;
+
+    auto operator=( Register && ) = delete;
+
+    auto operator=( Register const & ) = delete;
+
+    /**
+     * \brief Read the register.
+     *
+     * \return The register contents.
+     */
+    operator Type() const noexcept
+    {
+        return m_register;
+    }
+
+    /**
+     * \brief Assignment operator.
+     *
+     * \param[in] expression The expression to assign to the register.
+     *
+     * \return The assigned to object.
+     */
+    auto operator=( Type expression ) noexcept -> Register &
+    {
+        m_register = expression;
+
+        return *this;
+    }
+
+    /**
+     * \brief Bitwise AND assignment operator.
+     *
+     * \param[in] expression The expression to bitwise AND into the register.
+     *
+     * \return The assigned to object.
+     */
+    auto operator&=( Type expression ) noexcept -> Register &
+    {
+        m_register &= expression;
+
+        return *this;
+    }
+
+    /**
+     * \brief Bitwise OR assignment operator.
+     *
+     * \param[in] expression The expression to bitwise OR into the register.
+     *
+     * \return The assigned to object.
+     */
+    auto operator|=( Type expression ) noexcept -> Register &
+    {
+        m_register |= expression;
+
+        return *this;
+    }
+
+    /**
+     * \brief Bitwise XOR assignment operator.
+     *
+     * \param[in] expression The expression to bitwise XOR into the register.
+     *
+     * \return The assigned to object.
+     */
+    auto operator^=( Type expression ) noexcept -> Register &
+    {
+        m_register ^= expression;
+
+        return *this;
+    }
+
+  private:
+    /**
+     * \brief The register.
+     */
+    Type volatile m_register;
+};
+#endif // MICROLIBRARY_TARGET_IS_DEVELOPMENT_ENVIRONMENT
+
 } // namespace microlibrary
 
 #endif // MICROLIBRARY_REGISTER_H
