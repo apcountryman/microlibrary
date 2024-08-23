1edcee11be55872a12abad3671cdf667fc4a4715
Andrew Countryman <apcountryman@gmail.com>
2024-08-23 08:40:17 -0700

    Add Microchip automated testing facilities skeleton
    Resolves #283 (Add Microchip automated testing facilities skeleton).

---
+++ libraries/microlibrary/ANY/DEVELOPMENT_ENVIRONMENT/include/microlibrary/testing/automated/microchip.h
@@ -0,0 +1,32 @@
+/**
+ * microlibrary
+ *
+ * Copyright 2024, Andrew Countryman <apcountryman@gmail.com> and the microlibrary
+ * contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+/**
+ * \file
+ * \brief microlibrary::Testing::Automated::Microchip interface.
+ */
+
+#ifndef MICROLIBRARY_TESTING_AUTOMATED_MICROCHIP_H
+#define MICROLIBRARY_TESTING_AUTOMATED_MICROCHIP_H
+
+/**
+ * \brief Microchip automated testing facilities.
+ */
+namespace microlibrary::Testing::Automated::Microchip {
+} // namespace microlibrary::Testing::Automated::Microchip
+
+#endif // MICROLIBRARY_TESTING_AUTOMATED_MICROCHIP_H
