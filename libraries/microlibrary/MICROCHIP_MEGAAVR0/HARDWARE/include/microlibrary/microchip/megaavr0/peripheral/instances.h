1231ce370f2ff8430233891113009fe111ae0ee9
Andrew Countryman <apcountryman@gmail.com>
2024-08-23 11:29:16 -0700

    Add Microchip megaAVR 0-series peripheral instances skeleton
    Resolves #287 (Add Microchip megaAVR 0-series peripheral instances
    skeleton).

---
+++ libraries/microlibrary/MICROCHIP_MEGAAVR0/HARDWARE/include/microlibrary/microchip/megaavr0/peripheral/instances.h
@@ -0,0 +1,29 @@
+/**
+ * microlibrary
+ *
+ * Copyright 2024, Andrew Countryman <apcountryman@gmail.com> and the microlibrary
+ * contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+/**
+ * \file
+ * \brief microlibrary Microchip megaAVR 0-series peripheral instances interface.
+ */
+
+#ifndef MICROLIBRARY_MICROCHIP_MEGAAVR0_PERIPHERAL_INSTANCES_H
+#define MICROLIBRARY_MICROCHIP_MEGAAVR0_PERIPHERAL_INSTANCES_H
+
+namespace microlibrary::Microchip::megaAVR0::Peripheral {
+} // namespace microlibrary::Microchip::megaAVR0::Peripheral
+
+#endif // MICROLIBRARY_MICROCHIP_MEGAAVR0_PERIPHERAL_INSTANCES_H
